59568c5d8426cf2a7183498d71ee2f2a4c0db3a5
Thomas <dev@cookingeek.de>
2020-10-21 14:49:37 +0200

    add i18n functionality and app conf

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# sonos-kids-remote
+# Sonos Remote 4 Kids (sonos-kids-remote)
+
 Sonos remote with RFID tags for kids.
+NodeJS Application... 
+
+## Hardware
+
+* Raspberry Pi Zero W
+* RFID RC522 Module
+* Jumper Cable
+* Sonos Speaker
+
+## Installation
+
--- app.js
+++ app.js
@@ -3,12 +3,40 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var PropertiesReader = require('properties-reader');
+var prop = PropertiesReader('config/sonos-remote-4kids.properties');
 
+var i18next = require("i18next");
+var middleware = require('i18next-http-middleware');
+var FilesystemBackend = require('i18next-node-fs-backend');
+
+i18next.use(FilesystemBackend).use(middleware.LanguageDetector).init({
+ 	ns: 'resource',
+ 	defaultNS:'resource',
+	preload: ['de', 'en'],
+	supportedLngs: ['de','en'],
+    fallbackLng: 'de',
+	debug: true,
+    ignoreRoutes: ['/css', '/img', 'js', 'lib', 'views'],
+    resGetPath: __dirname + '/locales/{{lng}}/{{ns}}.json',
+ 	backend: {
+ 		loadPath: path.join(__dirname + '/locales/{{lng}}/{{ns}}.json')
+ 	}
+});
+ 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(
+  middleware.handle(i18next, {
+  })
+)
+
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static(__dirname + '/node_modules/jquery/dist'));
+app.use(express.static(__dirname + '/node_modules/@popperjs/core/dist/umd'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
